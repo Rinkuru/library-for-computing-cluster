35f36e87e4a6c1e4dfafd362a2f94d59fd23565e
Kurganskiy <l.kurganskiy>
2026-05-27 20:33:51 +0300

    fix benchmark

--- debug-build.sh
+++
@@ -1,2 +0,0 @@
-cmake -S . -B build-debug -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-cmake --build build-debug #--verbose
--- scripts/benchmark_simpleWorkers.sh
+++ scripts/benchmark_simpleWorkers.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/env bash
 
 LOGFILE="scripts/simpelTest.log"
-workers="8";
 
 echo > $LOGFILE
 
-./build/examples/integral_master --workers "$workers" >> $LOGFILE 2>&1 &
+./build/examples/integral_master --workers 1 >> $LOGFILE 2>&1 &
 
 master_pid=$!
-worker_pids=()
 
 start=$(date +%s%N)
 
-for ((i = 0; i < workers; ++i)); do
-    ./build/examples/integral_worker --threads 1 --cores 1 --first-core "$i" >> $LOGFILE 2>&1 &
-    worker_pids[$i]=$!
-done
+./build/examples/integral_worker --threads 1 --cores 1 --first-core "$i" >> $LOGFILE 2>&1 &
+worker_pid=$!
 
-for ((i = 0; i < workers; ++i)); do
-    wait "${worker_pids[$i]}"
-done
+wait "$worker_pid"
 wait "$master_pid"
 
 finish=$(date +%s%N)
 elapsed_ms=$(((finish - start) / 1000000))
+
+#здесь нужна какая-то проверка на то что тест проёден успешно, а точнее сравнение с числом пи
+
 
 echo "time: ${elapsed_ms} ms" >> $LOGFILE
 echo >> $LOGFILE
