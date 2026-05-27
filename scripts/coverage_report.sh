e7b1ef533956b0edc7a2b6f2d19c101619f45e39
Kurganskiy <l.kurganskiy>
2026-05-27 23:30:20 +0300

    fix style

--- scripts/coverage_report.sh
+++ scripts/coverage_report.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+GREEN="\033[32m"
+RESET="\033[0m"
 
 TITLE=${1}
 INFO_FILE="scripts/coverage.info"
@@ -7,11 +9,9 @@ FILTERED_INFO="scripts/coverage.filtered.info"
 lcov --capture --directory build-coverage --output-file "$INFO_FILE" --quiet
 lcov --extract "$INFO_FILE" "$PWD/src/*" "$PWD/examples/*" --output-file "$FILTERED_INFO" --quiet
 
-percent=$(lcov --summary "$FILTERED_INFO" 2>&1 | awk '/lines/ {print $2; exit}')
-
-if [ -z "$percent" ]; then
-    echo "coverage: unknown"
-    exit 1
-fi
-
-echo "$TITLE: $percent"
+echo "$TITLE:"
+echo -e "${GREEN}lines: "
+lcov --summary "$FILTERED_INFO" 2>&1 | awk '/lines/ {print $2; exit}'
+echo "functions: "
+lcov --summary "$FILTERED_INFO" 2>&1 | awk '/functions/ {print $2; exit}'
+echo -e "${RESET}"
